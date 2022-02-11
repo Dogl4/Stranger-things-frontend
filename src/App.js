7896dabac07c9184fa5bd22badfed355ec7e698d
Dogl4 <doougllas@hotmail.com.br>
2022-02-11 16:06:36 -0300

    :small_blue_diamond: 8. Cria uma variavel uma variavel de ambiente para desenvolvimento e adcional um tag.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
+const DEV = (process.env.REACT_APP_DEV === 'true');
+
 function App() {
   return (
     <div className="App">
+      { DEV ? <h1>Em Desenvolvimento</h1> : null }
       <StrangerThings />
     </div>
   );
